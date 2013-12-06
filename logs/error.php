91c6636d354ece1fc904aea265a37fcd42b4e1b7
PavanKotikalapudi <pavan0831@gmail.com>
2013-12-06 14:56:08 -0800

    commit

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-12-03 03:27:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-12-03T03:27:45+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-12-03T09:56:54+00:00	INFO	cookiefailure	Username and password do not match or you do not have an account yet.
+2013-12-03T09:58:44+00:00	INFO	cookiefailure	Username and password do not match or you do not have an account yet.
+2013-12-03T21:55:42+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
